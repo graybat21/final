38c4032d93717a377123f18dd5607f5e286371fc
H.euna <HEO@Hs-MacBook-Pro.local>
2017-05-01 18:06:47 +0900

    footer

--- src/main/java/net/kh/GuestHi/HomeController.java
+++ src/main/java/net/kh/GuestHi/HomeController.java
@@ -25,7 +25,6 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 //		Date date = new Date();
 //		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 //		
@@ -33,7 +32,7 @@ public class HomeController {
 //		
 //		model.addAttribute("serverTime", formattedDate );
 		
-		return "member/loginForm/join";
+		return "main/main/GH";
 	}
 	
 }
