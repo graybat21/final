8a6b852a02add48560133c6d86a2effd9e30e8f1
Hye <itoechan@naver.com>
2017-05-15 20:32:47 +0900

    mypage.css 수정

--- src/main/java/net/kh/GuestHi/HomeController.java
+++ src/main/java/net/kh/GuestHi/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 //		
 //		model.addAttribute("serverTime", formattedDate );
 		
-		return "guesthouse/roomdetail/MAIN";
+		return "mypage/modifyMyInfo/MAIN";
 
 	}
 	
