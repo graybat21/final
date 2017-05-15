8d08e9497f6827a23777ae85e2ffda6d4f4c7f68
graybat21 <graybat21@gmail.com>
2017-05-15 21:26:23 +0900

    no message

--- src/main/java/net/kh/mypage/MypageController.java
+++ src/main/java/net/kh/mypage/MypageController.java
@@ -5,23 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class MypageController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping("/myPage.gh")
 	public String noticeList(Model model) {
 		logger.info("MypageController - myPage.gh");
-		return "mypage/mypage/마이페이지";
+		return "mypage/modifyMyInfo/MY PAGE";
 	}
-	
+
 }
