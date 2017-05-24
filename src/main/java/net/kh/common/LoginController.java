1dac1feace733c5b607d796b685351ccc212d582
graybat21 <graybat21@gmail.com>
2017-05-24 19:26:08 +0900

    no message

--- src/main/java/net/kh/common/LoginController.java
+++ src/main/java/net/kh/common/LoginController.java
@@ -119,7 +119,7 @@ public class LoginController {
 		}
 		mav.addObject("member", new MemberVO());
 		mav.addObject("host", new HostVO());
-		mav.setViewName("main/main/LOGOUT");
+		mav.setViewName("redirect:/main.gh");
 		return mav;
 	}
 }
